6b5ccf219e7c02314047063aa42698eaa9e3a15c
TikaYulian27 <tikayulian27@gmail.com>
2020-04-09 13:37:40 +0700

    add API address in index.js

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,7 +1,21 @@
 const domainPath = 'http://loalhost:3001';
 const GetAPI = (path) => {
     const promise = new Promise ((resolve, reject)=>{
-        fetch 
-    }
-    )
+        fetch(`${domainPath}/${path}`)
+        .then(response => response.json())
+        .then((result) => {
+            resolve(result);
+            }, (err) => {
+                reject(err);
+        })
+    })
+    return promise;
 }
+
+const getNewsBlog = () => GetAPI ('posts?_sort=id&_order=dec');
+
+const API ={
+    getNewsBlog
+}
+
+export default API;
