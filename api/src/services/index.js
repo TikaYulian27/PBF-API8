804384434a0073f3bba4bee486ede33272f4480d
TikaYulian27 <tikayulian27@gmail.com>
2020-04-09 20:51:31 +0700

    add file

--- api/src/services/index.js
+++ api/src/services/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import getAPI from '../../Get';
-import PostAPI from '../../Post';
-import DeleteAPI from '../../Delete';
+import getAPI from './Get';
+import PostAPI from './Post';
+import DeleteAPI from './Delete';
 
 const getNewsBlog = () => getAPI("posts?_sort=id&_order=asc")
 const postNewsBlog = (dataYangDikirim) => PostAPI("posts", dataYangDikirim)
@@ -14,3 +14,7 @@ const API = {
 }
 
 export default API;
+
+
+
+
